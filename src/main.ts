110602b81dde96dbb39e1c4e7484935708357322
Dinmuhammad Qosimov <qosimovmirjalol829@gmail.com>
2025-09-26 17:13:02 +0500

    test

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const port = Number(process.env.PORT);
   const app = await NestFactory.create(AppModule);
-  await app.listen(port, () => console.log('server running on port', port));
+  await app.listen(port, () => console.log('server running on port --->', port));
 }
 bootstrap();
